5b02a14009c6830122f6d44eb07a9dc98da67fb9
Audrey Baniant <audrey.baniant@gmail.com>
2023-08-15 14:22:59 -0400

    Can update taks

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   get '/tasks/:id', to: 'tasks#show', as: :task
   post '/tasks', to: 'tasks#create'
+  patch '/tasks/:id', to: 'tasks#update'
 end
